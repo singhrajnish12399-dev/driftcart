c3387f6d28781a820e958718ff593f27c2364675
singhrajnish12399-dev <singhrajnish12399@gmail.com>
2026-06-04 10:02:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 function App() {
+  const products = [
+    { id: 1, name: "T-Shirt", price: "₹499" },
+    { id: 2, name: "Shoes", price: "₹1499" },
+    { id: 3, name: "Watch", price: "₹999" },
+    { id: 4, name: "Headphones", price: "₹1999" }
+  ];
+
   return (
     <div>
       <h1>DriftCart</h1>
-      <p>My first Vercel app is working!</p>
+      <p>Welcome to my shopping app</p>
+
+      {products.map((product) => (
+        <div key={product.id}>
+          <h3>{product.name}</h3>
+          <p>{product.price}</p>
+          <button>Add to Cart</button>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
