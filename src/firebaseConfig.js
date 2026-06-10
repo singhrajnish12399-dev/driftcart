82af239fc625fcb155c0c2c005e3e62f46235b7c
singhrajnish12399-dev <singhrajnish12399@gmail.com>
2026-06-10 12:43:38 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// ⚠️ Firebase Console se mila hua asli data yahan daalna hai:
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "driftcart-XXXXX.firebaseapp.com",
-  projectId: "driftcart-XXXXX",
-  storageBucket: "driftcart-XXXXX.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AAaaBbBbCcCcDdDd_YOUR_REAL_KEY", // Apni asli Key paste karo
+  authDomain: "driftcart-apna-id.firebaseapp.com", // Apna asli ID
+  projectId: "driftcart-apna-id", // Apna asli ID
+  storageBucket: "driftcart-apna-id.appspot.com",
+  messagingSenderId: "123456789012", // Asli number
+  appId: "1:123456789012:web:abcdef123456" // Asli App ID
 };
 
 const app = initializeApp(firebaseConfig);
